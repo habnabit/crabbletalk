0e13a17d3bf871ae9f9ee6abe614a02da615be38
hab <_@habnab.it>
2022-03-25 16:09:54 -0700

    fifo? idk

--- crabbletalk-afpd/src/bin/afpd.rs
+++ crabbletalk-afpd/src/bin/afpd.rs
@@ -4,21 +4,14 @@ use anyhow::{anyhow, Context, Result};
 use crabbletalk::link::decode_appletalk;
 use pnet::packet::Packet;
 
-fn pcap_path() -> PathBuf {
-    let now = chrono::offset::Utc::now();
-    format!("atalk-{}.pcap", now.format("%Y%m%d_%H%M%S")).into()
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let sock_path: PathBuf = args[1].parse()?;
     let router_path: PathBuf = args[2].parse()?;
-    let pcap_path = pcap_path();
     let sock = tokio::net::UnixDatagram::bind(&sock_path)?;
     sock.connect(&router_path)?;
     sock.send(b"").await?;
-    let mut pcap_writer = pcap_file::PcapWriter::new(std::fs::File::create(&pcap_path)?)?;
     let mut buf = vec![0u8; 1600];
 
     loop {
@@ -26,17 +19,10 @@ async fn main() -> Result<()> {
             _ = tokio::signal::ctrl_c() => { break }
             r = sock.recv_from(&mut buf) => { r }
         }?;
-        let now = chrono::offset::Utc::now();
         let data = &buf[..n_read];
         if let Some(p) = decode_appletalk(data) {
             println!("read {} bytes: {:#?}\n  {:?}", n_read, p, p.payload());
         }
-        pcap_writer.write(
-            now.timestamp() as u32,
-            now.timestamp_subsec_nanos(),
-            data,
-            n_read as u32,
-        )?;
     }
 
     std::fs::remove_file(&sock_path).with_context(|| format!("whilst deleting {:?}", sock_path))?;
--- crabbletalk-afpd/src/bin/router.rs
+++ crabbletalk-afpd/src/bin/router.rs
@@ -1,14 +1,52 @@
-use std::{path::PathBuf, collections::BTreeSet};
+use std::{collections::BTreeSet, fs::File, path::PathBuf};
 
 use anyhow::{anyhow, Context, Result};
+use clap::Parser;
+use nix::sys::stat::Mode;
+use pcap_file::PcapWriter;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(short, long)]
+    pcap: Option<PathBuf>,
+    socket_path: PathBuf,
+    #[clap(short = 'f', long)]
+    mkfifo: bool,
+}
+
+impl Cli {
+    fn pcap_writer(&self) -> Result<Option<PcapWriter<File>>> {
+        let p = match &self.pcap {
+            Some(p) => p,
+            None => return Ok(None),
+        };
+        let f = if self.mkfifo {
+            nix::unistd::mkfifo(p, Self::fifo_mode())?;
+            std::fs::OpenOptions::new().write(true).open(p)?
+        } else {
+            File::create(p)?
+        };
+        Ok(Some(PcapWriter::new(f)?))
+    }
+
+    fn fifo_mode() -> Mode {
+        Mode::S_IRUSR
+            | Mode::S_IWUSR
+            | Mode::S_IRGRP
+            | Mode::S_IWGRP
+            | Mode::S_IROTH
+            | Mode::S_IWOTH
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args: Vec<_> = std::env::args().collect();
-    let sock_path: PathBuf = args[1].parse()?;
-    let listener = tokio::net::UnixDatagram::bind(&sock_path)?;
+    let cli = Cli::parse();
+    let listener = tokio::net::UnixDatagram::bind(&cli.socket_path)?;
     let mut clients = BTreeSet::new();
     let mut buf = vec![0u8; 1600];
+    let mut pcap_writer = cli.pcap_writer()?;
 
     loop {
         let (n_read, addr) = tokio::select! {
@@ -17,13 +55,24 @@ async fn main() -> Result<()> {
         }?;
         let data = &buf[..n_read];
         println!("read {} bytes from {:?}", n_read, addr);
+        if let Some(writer) = &mut pcap_writer {
+            let now = chrono::offset::Utc::now();
+            writer.write(
+                now.timestamp() as u32,
+                now.timestamp_subsec_nanos(),
+                data,
+                n_read as u32,
+            )?;
+        }
         if let Some(path) = addr.as_pathname() {
             if !clients.contains(path) {
                 clients.insert(path.to_owned());
             }
             let mut to_remove = BTreeSet::new();
             for client in &clients {
-                if client == path { continue }
+                if client == path {
+                    continue;
+                }
                 match listener.send_to(data, client).await {
                     Ok(_) => {}
                     Err(e) => {
@@ -38,7 +87,8 @@ async fn main() -> Result<()> {
         }
     }
 
-    std::fs::remove_file(&sock_path).with_context(|| format!("whilst deleting {:?}", sock_path))?;
+    std::fs::remove_file(&cli.socket_path)
+        .with_context(|| format!("whilst deleting {:?}", &cli.socket_path))?;
 
     Ok(())
 }
